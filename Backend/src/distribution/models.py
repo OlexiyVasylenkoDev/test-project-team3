93cf9f7c22ba7456b636a93f87253b233acd1508
Baclrary <ihorprotsak@gmail.com>
2023-05-16 17:37:36 +0300

    add distribution model

---
+++ Backend/src/distribution/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+# from django.core.mail import send_mail
+from django.contrib.auth.models import User
+
+
+class EmailDistribution(models.Model):
+    subject = models.CharField(max_length=200)
+    message = models.TextField(max_length=3000)
+    sent = models.BooleanField(default=False)

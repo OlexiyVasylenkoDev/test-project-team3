b56a2576211b1ff34fede9b73394127576124912
JustOdessa <alexeyvasilenkoo@gmail.com>
2023-05-16 21:31:29 +0300

    Add admin page

--- Backend/src/core/admin.py
+++ Backend/src/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from core.models import User, SellerProfile, BuyerProfile
+from .models import User, SellerProfile, BuyerProfile
 
 admin.site.register([User, SellerProfile, BuyerProfile])

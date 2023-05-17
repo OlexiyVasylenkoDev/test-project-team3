7eecbf181ad7398ff38d53c7602f4fb0e718015a
Baclrary <ihorprotsak@gmail.com>
2023-05-17 14:01:20 +0300

    add .env_example

--- Backend/src/config/settings.py
+++ Backend/src/config/settings.py
@@ -85,20 +85,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
-try:
-    from local_settings import *
-except ImportError:
-    pass
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 

0b33e835666cd086b111a1e130591353c4e9f8d6
Baclrary <ihorprotsak@gmail.com>
2023-05-18 11:23:38 +0300

    add review api permissions

--- Backend/src/config/settings.py
+++ Backend/src/config/settings.py
@@ -141,9 +141,5 @@ AUTH_USER_MODEL = "core.User"
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
+    ]
 }
--- Backend/src/review/views.py
+++ Backend/src/review/views.py
@@ -1,5 +1,6 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from permissions import IsAuthenticated
 
 from .models import Review
 from .serializers import ReviewSerializer
@@ -12,3 +13,17 @@ class ReviewViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+    def update(self, request, *args, **kwargs):
+        review = self.get_object()
+        if review.user != request.user:
+            return Response({'message': 'Not found', 'status': '404'}, status=status.HTTP_404_NOT_FOUND, )
+        return super(ReviewViewSet, self).update(request, *args, **kwargs)
+
+
+    def destroy(self, request, *args, **kwargs):
+        review = self.get_object()
+        if review.user != request.user:
+            return Response({'message': 'Not found', 'status': '404'}, status=status.HTTP_404_NOT_FOUND, )
+        return super(ReviewViewSet, self).destroy(request, *args, **kwargs)

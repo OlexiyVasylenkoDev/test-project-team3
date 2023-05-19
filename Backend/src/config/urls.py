76f6366682d2c415a21fccc088bc89b6283ff2b6
Baclrary <ihorprotsak@gmail.com>
2023-05-19 16:08:56 +0300

    return authentication api to urls

--- Backend/src/config/urls.py
+++ Backend/src/config/urls.py
@@ -23,6 +23,7 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
+from core.urls import urlpatterns as core_urls
 
 main_router = routers.DefaultRouter()
 main_router.register(r'category', CategoryViewSet)
@@ -36,6 +37,7 @@ urlpatterns = [
     # path('', ),
     path('admin/', admin.site.urls),
     path('api/v1/', include(main_router.urls)),
+    path('api/v1/authentication/', include('core.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),

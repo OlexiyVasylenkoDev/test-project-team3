f12e2e7c932f0dd354878d6e263f47fc28e38995
Baclrary <ihorprotsak@gmail.com>
2023-05-19 15:55:39 +0300

    add distributions api

--- Backend/src/distribution/models.py
+++ Backend/src/distribution/models.py
@@ -1,8 +1,18 @@
 from django.db import models
-# from django.core.mail import send_mail
-from django.contrib.auth.models import User
 
+class DistributionCategory(models.Model):
+    title = models.CharField(max_length=120)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
 
 class EmailDistribution(models.Model):
+    category = models.ForeignKey(DistributionCategory, on_delete=models.CASCADE)
     subject = models.CharField(max_length=200)
     message = models.TextField(max_length=3000)
+
+    def __str__(self):
+        return f"{self.category} - {self.subject}"

14ab95a3e3fd4cd8dfaa144663aa4aecee796380
Baclrary <ihorprotsak@gmail.com>
2023-05-19 15:56:00 +0300

    reorganise projects urls

--- Backend/src/config/urls.py
+++ Backend/src/config/urls.py
@@ -1,26 +1,14 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework import permissions, routers
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from catalog.urls import router
+from catalog.views import CategoryViewSet
+from catalog.views import ProductViewSet
+from review.views import ReviewViewSet
+from distribution.views import DistributionCategoryViewSet, EmailDistributionViewSet
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -35,11 +23,19 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
+
+main_router = routers.DefaultRouter()
+main_router.register(r'category', CategoryViewSet)
+main_router.register(r'product', ProductViewSet)
+main_router.register(r'review', ReviewViewSet)
+main_router.register(r'distribution', EmailDistributionViewSet)
+main_router.register(r'distribution_categories', DistributionCategoryViewSet)
+
+
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', ),
     path('admin/', admin.site.urls),
-    path('api/v1/review/', include("review.urls")),
-    path('api/v1/authentication/', include("core.urls")),
+    path('api/v1/', include(main_router.urls)),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),

54fcef067252018adfbbdc8b6f228b6fca005661
JustOdessa <alexeyvasilenkoo@gmail.com>
2023-05-22 13:48:27 +0300

    Add permissions to attributes

--- Backend/src/catalog/views.py
+++ Backend/src/catalog/views.py
@@ -26,6 +26,13 @@ class CategoryAttributeViewSet(ModelViewSet):
     queryset = CategoryAttribute.objects.all()
     serializer_class = CategoryAttributeSerializer
 
+    def get_permissions(self):
+        if self.action in ('create', 'update', 'partial_update', 'destroy'):
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = []
+        return [permission() for permission in permission_classes]
+
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
@@ -47,3 +54,12 @@ class ProductViewSet(ModelViewSet):
 class ProductAttributeViewSet(ModelViewSet):
     queryset = ProductAttribute.objects.all()
     serializer_class = ProductAttributeSerializer
+
+    def get_permissions(self):
+        if self.action in ('create', 'destroy'):
+            permission_classes = [IsSeller]
+        elif self.action in ("update", "partial_update"):
+            permission_classes = [IsProductSeller]
+        else:
+            permission_classes = []
+        return [permission() for permission in permission_classes]

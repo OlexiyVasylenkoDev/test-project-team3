c548c7ec079223ab3bec67fc21c36dde7fdbfba1
JustOdessa <alexeyvasilenkoo@gmail.com>
2023-05-25 16:21:01 +0300

    Frontend works with docker. Need to fix reload still

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function App() {
-  return <div className="App">Here will be Rozetka 2.0</div>;
+  return <div className="App">Here will be Rozetka 2.0 Docker</div>;
 }
 
 export default App;
